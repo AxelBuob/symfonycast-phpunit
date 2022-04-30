ae4b932cd4f6692b77e2062bbe65ab04bad4d2f6
Axel Buob <contact@axelbuob.fr>
2022-04-30 10:25:55 +0200

    Chapter 02

---
+++ src/Entity/Dinosaur.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DinosaurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DinosaurRepository::class)]
+class Dinosaur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $length = 0;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLength(): ?int
+    {
+        return $this->length;
+    }
+
+    public function setLength(int $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+}
---
+++ tests/Entity/DinosaurTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Entity;
+
+use PHPUnit\Framework\TestCase;
+use App\Entity\Dinosaur;
+
+class DinosaurTest extends TestCase
+{
+    public function testSettingLength()
+    {
+        $dinosaur = new Dinosaur();
+        $this->assertSame(0, $dinosaur->getLength());
+
+        $dinosaur->setLength(9);
+        $this->assertSame(9, $dinosaur->getLength());
+    }
+
+    public function testDinosaurHasNotShrunk()
+    {
+        $dinosaur = new Dinosaur();
+        $dinosaur->setLength(15);
+
+        $this->assertGreaterThan(16, $dinosaur->getLength(), 'Did you put it in the washing machine?');
+    }
+}
