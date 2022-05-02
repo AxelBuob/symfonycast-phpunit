fcfc4ae1e44f10e851818c782e5791ebb2331d50
Axel Buob <contact@axelbuob.fr>
2022-05-02 10:26:13 +0200

    chapter 9

--- src/Entity/Enclosure.php
+++ src/Entity/Enclosure.php
@@ -6,11 +6,16 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity]
-#[ORM\Table(name: "enclosure")]
+#[ORM\Entity(repositoryClass: EnclosureRepository::class)]
 class Enclosure
 {
-    #[ORM\OneToMany(targetEntity:"App\Entity\Dinosaur", mappedBy: "enclosure", casade: "persist")]
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\OneToMany(targetEntity:"App\Entity\Dinosaur", mappedBy: "enclosure", cascade: ["persist"])]
     private $dinosaurs;
 
     public function __construct()
@@ -21,5 +26,10 @@ class Enclosure
     public function getDinosaurs(): Collection
     {
         return $this->dinosaurs;
+    }
+
+    public function addDinosaur(Dinosaur $dinosaur): void
+    {
+        $this->dinosaurs[] = $dinosaur;
     }
 }
--- tests/Entity/EnclosureTest.php
+++ tests/Entity/EnclosureTest.php
@@ -11,13 +11,17 @@ class EnclosureTest extends TestCase
     public function testItHasNoDinosaursByDefault()
     {
         $enclosure = new Enclosure();
-        $this->assertCount(0, $enclosure->getDinosaurs());
+        $this->assertEmpty($enclosure->getDinosaurs());
     }
 
     public function testItAddsDinosaurs()
     {
         $enclosure = new Enclosure();
-        $this->markTestIncomplete('Waiting for confirmation from GenLab');
-        //$enclosure->addDinosaur(new Dinosaur());
+
+        $enclosure->addDinosaur(new Dinosaur());
+        $enclosure->addDinosaur(new Dinosaur());
+
+        $this->assertCount(2, $enclosure->getDinosaurs());
     }
+
 }
